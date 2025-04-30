1564c8d6d2d1ed21ded05be5e0663bc0516a37a3
Ahmed <Ahmed@DESKTOP-DN0KBIA>
2025-04-30 20:49:12 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace CaptchaGen
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            btnVerifyText = new Button();
+            btnRefreshText = new Button();
+            txtTextCaptcha = new TextBox();
+            label1 = new Label();
+            picTextCaptcha = new PictureBox();
+            tabPage2 = new TabPage();
+            btnVerifyMath = new Button();
+            btnRefreshMath = new Button();
+            txtMathCaptcha = new TextBox();
+            lblMathCaptcha = new Label();
+            tabPage3 = new TabPage();
+            btnVerifyImage = new Button();
+            btnRefreshImage = new Button();
+            picImageContainer = new Panel();
+            lblImageChallenge = new Label();
+            tabPage4 = new TabPage();
+            btnRecaptchaVerify = new Button();
+            label2 = new Label();
+            chkRecaptcha = new CheckBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)picTextCaptcha).BeginInit();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            picImageContainer.SuspendLayout();
+            tabPage4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Margin = new Padding(4, 5, 4, 5);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(645, 555);
+            tabControl1.TabIndex = 0;
+            tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnVerifyText);
+            tabPage1.Controls.Add(btnRefreshText);
+            tabPage1.Controls.Add(txtTextCaptcha);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(picTextCaptcha);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Margin = new Padding(4, 5, 4, 5);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(4, 5, 4, 5);
+            tabPage1.Size = new Size(637, 522);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Text CAPTCHA";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // btnVerifyText
+            // 
+            btnVerifyText.Location = new Point(290, 300);
+            btnVerifyText.Margin = new Padding(4, 5, 4, 5);
+            btnVerifyText.Name = "btnVerifyText";
+            btnVerifyText.Size = new Size(100, 35);
+            btnVerifyText.TabIndex = 4;
+            btnVerifyText.Text = "Verify";
+            btnVerifyText.UseVisualStyleBackColor = true;
+            btnVerifyText.Click += btnVerifyText_Click;
+            // 
+            // btnRefreshText
+            // 
+            btnRefreshText.Location = new Point(182, 300);
+            btnRefreshText.Margin = new Padding(4, 5, 4, 5);
+            btnRefreshText.Name = "btnRefreshText";
+            btnRefreshText.Size = new Size(100, 35);
+            btnRefreshText.TabIndex = 3;
+            btnRefreshText.Text = "Refresh";
+            btnRefreshText.UseVisualStyleBackColor = true;
+            btnRefreshText.Click += btnRefreshText_Click;
+            // 
+            // txtTextCaptcha
+            // 
+            txtTextCaptcha.Location = new Point(185, 260);
+            txtTextCaptcha.Margin = new Padding(4, 5, 4, 5);
+            txtTextCaptcha.Name = "txtTextCaptcha";
+            txtTextCaptcha.Size = new Size(207, 27);
+            txtTextCaptcha.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(181, 227);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Enter the text from the image left:";
+            // 
+            // picTextCaptcha
+            // 
+            picTextCaptcha.Anchor = AnchorStyles.None;
+            picTextCaptcha.BorderStyle = BorderStyle.FixedSingle;
+            picTextCaptcha.Location = new Point(196, 136);
+            picTextCaptcha.Margin = new Padding(4, 5, 4, 5);
+            picTextCaptcha.Name = "picTextCaptcha";
+            picTextCaptcha.Size = new Size(191, 76);
+            picTextCaptcha.TabIndex = 0;
+            picTextCaptcha.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btnVerifyMath);
+            tabPage2.Controls.Add(btnRefreshMath);
+            tabPage2.Controls.Add(txtMathCaptcha);
+            tabPage2.Controls.Add(lblMathCaptcha);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Margin = new Padding(4, 5, 4, 5);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(4, 5, 4, 5);
+            tabPage2.Size = new Size(637, 522);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Math CAPTCHA";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnVerifyMath
+            // 
+            btnVerifyMath.Location = new Point(260, 185);
+            btnVerifyMath.Margin = new Padding(4, 5, 4, 5);
+            btnVerifyMath.Name = "btnVerifyMath";
+            btnVerifyMath.Size = new Size(100, 35);
+            btnVerifyMath.TabIndex = 3;
+            btnVerifyMath.Text = "Verify";
+            btnVerifyMath.UseVisualStyleBackColor = true;
+            btnVerifyMath.Click += btnVerifyMath_Click;
+            // 
+            // btnRefreshMath
+            // 
+            btnRefreshMath.Location = new Point(152, 185);
+            btnRefreshMath.Margin = new Padding(4, 5, 4, 5);
+            btnRefreshMath.Name = "btnRefreshMath";
+            btnRefreshMath.Size = new Size(100, 35);
+            btnRefreshMath.TabIndex = 2;
+            btnRefreshMath.Text = "Refresh";
+            btnRefreshMath.UseVisualStyleBackColor = true;
+            btnRefreshMath.Click += btnRefreshMath_Click;
+            // 
+            // txtMathCaptcha
+            // 
+            txtMathCaptcha.Location = new Point(152, 145);
+            txtMathCaptcha.Margin = new Padding(4, 5, 4, 5);
+            txtMathCaptcha.Name = "txtMathCaptcha";
+            txtMathCaptcha.Size = new Size(207, 27);
+            txtMathCaptcha.TabIndex = 1;
+            // 
+            // lblMathCaptcha
+            // 
+            lblMathCaptcha.AutoSize = true;
+            lblMathCaptcha.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMathCaptcha.Location = new Point(147, 92);
+            lblMathCaptcha.Margin = new Padding(4, 0, 4, 0);
+            lblMathCaptcha.Name = "lblMathCaptcha";
+            lblMathCaptcha.Size = new Size(85, 29);
+            lblMathCaptcha.TabIndex = 0;
+            lblMathCaptcha.Text = "1 + 2 =";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(btnVerifyImage);
+            tabPage3.Controls.Add(btnRefreshImage);
+            tabPage3.Controls.Add(picImageContainer);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Margin = new Padding(4, 5, 4, 5);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(4, 5, 4, 5);
+            tabPage3.Size = new Size(637, 522);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Image CAPTCHA";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // btnVerifyImage
+            // 
+            btnVerifyImage.Location = new Point(260, 446);
+            btnVerifyImage.Margin = new Padding(4, 5, 4, 5);
+            btnVerifyImage.Name = "btnVerifyImage";
+            btnVerifyImage.Size = new Size(100, 35);
+            btnVerifyImage.TabIndex = 2;
+            btnVerifyImage.Text = "Verify";
+            btnVerifyImage.UseVisualStyleBackColor = true;
+            btnVerifyImage.Click += btnVerifyImage_Click;
+            // 
+            // btnRefreshImage
+            // 
+            btnRefreshImage.Location = new Point(152, 446);
+            btnRefreshImage.Margin = new Padding(4, 5, 4, 5);
+            btnRefreshImage.Name = "btnRefreshImage";
+            btnRefreshImage.Size = new Size(100, 35);
+            btnRefreshImage.TabIndex = 1;
+            btnRefreshImage.Text = "Refresh";
+            btnRefreshImage.UseVisualStyleBackColor = true;
+            btnRefreshImage.Click += btnRefreshImage_Click;
+            // 
+            // picImageContainer
+            // 
+            picImageContainer.BorderStyle = BorderStyle.FixedSingle;
+            picImageContainer.Controls.Add(lblImageChallenge);
+            picImageContainer.Location = new Point(8, 9);
+            picImageContainer.Margin = new Padding(4, 5, 4, 5);
+            picImageContainer.Name = "picImageContainer";
+            picImageContainer.Size = new Size(618, 427);
+            picImageContainer.TabIndex = 3;
+            // 
+            // lblImageChallenge
+            // 
+            lblImageChallenge.AutoSize = true;
+            lblImageChallenge.Location = new Point(13, 15);
+            lblImageChallenge.Margin = new Padding(4, 0, 4, 0);
+            lblImageChallenge.Name = "lblImageChallenge";
+            lblImageChallenge.Size = new Size(202, 20);
+            lblImageChallenge.TabIndex = 0;
+            lblImageChallenge.Text = "Select all images with: object";
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(btnRecaptchaVerify);
+            tabPage4.Controls.Add(label2);
+            tabPage4.Controls.Add(chkRecaptcha);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Margin = new Padding(4, 5, 4, 5);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(4, 5, 4, 5);
+            tabPage4.Size = new Size(637, 522);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "reCAPTCHA";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // btnRecaptchaVerify
+            // 
+            btnRecaptchaVerify.Location = new Point(260, 185);
+            btnRecaptchaVerify.Margin = new Padding(4, 5, 4, 5);
+            btnRecaptchaVerify.Name = "btnRecaptchaVerify";
+            btnRecaptchaVerify.Size = new Size(100, 35);
+            btnRecaptchaVerify.TabIndex = 2;
+            btnRecaptchaVerify.Text = "Verify";
+            btnRecaptchaVerify.UseVisualStyleBackColor = true;
+            btnRecaptchaVerify.Click += btnRecaptchaVerify_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(147, 92);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(332, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Please check the box to prove you're not a robot:";
+            // 
+            // chkRecaptcha
+            // 
+            chkRecaptcha.Appearance = Appearance.Button;
+            chkRecaptcha.AutoSize = true;
+            chkRecaptcha.Location = new Point(151, 117);
+            chkRecaptcha.Margin = new Padding(4, 5, 4, 5);
+            chkRecaptcha.Name = "chkRecaptcha";
+            chkRecaptcha.Size = new Size(303, 30);
+            chkRecaptcha.TabIndex = 0;
+            chkRecaptcha.Text = "☐  I'm not a robot (reCAPTCHA simulation)";
+            chkRecaptcha.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(645, 555);
+            Controls.Add(tabControl1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "CAPTCHA Generator";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)picTextCaptcha).EndInit();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            picImageContainer.ResumeLayout(false);
+            picImageContainer.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox picTextCaptcha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTextCaptcha;
+        private System.Windows.Forms.Button btnRefreshText;
+        private System.Windows.Forms.Button btnVerifyText;
+        private System.Windows.Forms.Label lblMathCaptcha;
+        private System.Windows.Forms.TextBox txtMathCaptcha;
+        private System.Windows.Forms.Button btnRefreshMath;
+        private System.Windows.Forms.Button btnVerifyMath;
+        private System.Windows.Forms.Panel picImageContainer;
+        private System.Windows.Forms.Button btnRefreshImage;
+        private System.Windows.Forms.Button btnVerifyImage;
+        private System.Windows.Forms.CheckBox chkRecaptcha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRecaptchaVerify;
+        private System.Windows.Forms.Label lblImageChallenge;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CaptchaGen
+{
+    public partial class Form1 : Form
+    {
+        // Text CAPTCHA
+        private string currentTextCaptcha;
+
+        // Math CAPTCHA
+        private int currentMathCaptchaResult;
+
+        // Image CAPTCHA
+        private List<string> imageCategories = new List<string> { "car", "tree", "house", "dog", "cat", "bicycle" };
+        private string currentImageChallenge = "";
+        private List<PictureBox> imageBoxes = new List<PictureBox>();
+        private List<string> correctImages = new List<string>();
+        private List<string> selectedImages = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateTextCaptcha();
+            currentMathCaptchaResult = 0;
+        }
+
+        #region Text CAPTCHA
+        private void GenerateTextCaptcha()
+        {
+            var random = new Random();
+            const string chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
+            currentTextCaptcha = new string(Enumerable.Repeat(chars, random.Next(5, 8))
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            var bmp = new Bitmap(picTextCaptcha.Width, picTextCaptcha.Height);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                for (int i = 0; i < 50; i++)
+                {
+                    int x = random.Next(bmp.Width);
+                    int y = random.Next(bmp.Height);
+                    bmp.SetPixel(x, y, Color.FromArgb(random.Next()));
+                }
+
+                var font = new Font("Arial", 18, FontStyle.Bold);
+                var brush = new SolidBrush(Color.Black);
+
+                for (int i = 0; i < currentTextCaptcha.Length; i++)
+                {
+                    float x = 10 + (i * 20);
+                    float y = 10 + (float)(random.NextDouble() * 10 - 5);
+                    g.DrawString(currentTextCaptcha[i].ToString(), font, brush, x, y);
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    g.DrawLine(Pens.Gray,
+                        random.Next(bmp.Width), random.Next(bmp.Height),
+                        random.Next(bmp.Width), random.Next(bmp.Height));
+                }
+            }
+
+            picTextCaptcha.Image = bmp;
+        }
+
+        private void btnVerifyText_Click(object sender, EventArgs e)
+        {
+            if (txtTextCaptcha.Text.Equals(currentTextCaptcha, StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("Text CAPTCHA verification successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Text CAPTCHA verification failed. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefreshText_Click(object sender, EventArgs e)
+        {
+            GenerateTextCaptcha();
+        }
+        #endregion
+
+        #region Math CAPTCHA
+        private void GenerateMathCaptcha()
+        {
+            var random = new Random();
+            int num1 = random.Next(1, 10);
+            int num2 = random.Next(1, 10);
+            currentMathCaptchaResult = num1 + num2;
+            lblMathCaptcha.Text = $"{num1} + {num2} = ?";
+        }
+
+        private void btnVerifyMath_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMathCaptcha.Text, out int result) && result == currentMathCaptchaResult)
+            {
+                MessageBox.Show("Math CAPTCHA verification successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Math CAPTCHA verification failed. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefreshMath_Click(object sender, EventArgs e)
+        {
+            GenerateMathCaptcha();
+        }
+        #endregion
+
+        #region Image CAPTCHA
+        private void GenerateImageCaptcha()
+        {
+            selectedImages.Clear();
+
+            foreach (var box in imageBoxes)
+            {
+                picImageContainer.Controls.Remove(box);
+            }
+            imageBoxes.Clear();
+
+            var random = new Random();
+            currentImageChallenge = imageCategories[random.Next(imageCategories.Count)];
+            lblImageChallenge.Text = $"Select all images with: {currentImageChallenge}";
+
+            correctImages.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                correctImages.Add(currentImageChallenge);
+            }
+
+            int size = 80;
+            int margin = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                var box = new PictureBox
+                {
+                    Width = size,
+                    Height = size,
+                    Left = margin + (i % 3) * (size + margin),
+                    Top = 40 + (i / 3) * (size + margin),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Tag = i < 3 ? currentImageChallenge :
+                          imageCategories[random.Next(imageCategories.Count)]
+                };
+
+                box.Image = GetSampleImage(box.Tag.ToString());
+                box.Click += ImageBox_Click;
+
+                picImageContainer.Controls.Add(box);
+                imageBoxes.Add(box);
+            }
+        }
+
+        private Image GetSampleImage(string category)
+        {
+            try
+            {
+                // Try to load real image if exists
+                string imagePath = Path.Combine(Application.StartupPath, "Images", $"{category}.jpg");
+                if (File.Exists(imagePath))
+                {
+                    return Image.FromFile(imagePath);
+                }
+            }
+            catch { }
+
+            // Fallback to generated image
+            var bmp = new Bitmap(80, 80);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                var colors = new Dictionary<string, Color>
+                {
+                    {"car", Color.Red},
+                    {"tree", Color.Green},
+                    {"house", Color.Blue},
+                    {"dog", Color.Yellow},
+                    {"cat", Color.Orange},
+                    {"bicycle", Color.Purple}
+                };
+
+                g.Clear(colors.ContainsKey(category) ? colors[category] : Color.Gray);
+                g.DrawString(category, new Font("Arial", 8), Brushes.Black, 10, 30);
+            }
+            return bmp;
+        }
+
+        private void ImageBox_Click(object sender, EventArgs e)
+        {
+            var box = (PictureBox)sender;
+            string category = box.Tag.ToString();
+
+            if (box.BorderStyle == BorderStyle.Fixed3D)
+            {
+                box.BorderStyle = BorderStyle.FixedSingle;
+                selectedImages.Remove(category);
+            }
+            else
+            {
+                box.BorderStyle = BorderStyle.Fixed3D;
+                selectedImages.Add(category);
+            }
+        }
+
+        private void btnVerifyImage_Click(object sender, EventArgs e)
+        {
+            bool allCorrect = correctImages.All(img => selectedImages.Contains(img));
+            bool noIncorrect = selectedImages.All(img => correctImages.Contains(img));
+
+            if (allCorrect && noIncorrect && selectedImages.Count > 0)
+            {
+                MessageBox.Show("Image CAPTCHA verification successful!", "Success",
+                               MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Image CAPTCHA verification failed. Please try again.",
+                               "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefreshImage_Click(object sender, EventArgs e)
+        {
+            GenerateImageCaptcha();
+        }
+        #endregion
+
+        #region reCAPTCHA
+        private void btnRecaptchaVerify_Click(object sender, EventArgs e)
+        {
+            if (chkRecaptcha.Checked)
+            {
+                MessageBox.Show("reCAPTCHA verification successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please check the reCAPTCHA checkbox.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (tabControl1.SelectedIndex)
+            {
+                case 0: // Text
+                    GenerateTextCaptcha();
+                    break;
+                case 1: // Math
+                    GenerateMathCaptcha();
+                    break;
+                case 2: // Image
+                    GenerateImageCaptcha();
+                    break;
+                case 3: // reCAPTCHA
+                    break;
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
